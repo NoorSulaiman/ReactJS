65384be6be0e79edba79a8cfbb2d4043636cd3a4
NoorSulaiman <noor.s.suliman@gmail.com>
2018-10-13 22:33:23 -0400

    creating a basic react-dom module

---
+++ src/lib/react-dom.js
@@ -0,0 +1,88 @@
+import { isClass, isEvent, isFunction, isString } from './reakt-utils.js'
+
+//check the diffirance between 
+function diffCheck(vDom, dom) {
+    const isNewChildren = vDom.children.length !== dom.childNodes.length;
+
+    if (isNewChildren) {
+        dom.appendChild(
+            renderNode(vDom.children[vDom.children.length - 1])
+        );
+        return dom;
+    } else { return dom; }
+}
+
+function updateComponent(component) {
+    const oldBase = component.base;
+    const vDom = component.render();
+
+    component.base = diffCheck(vDom, oldBase)
+}
+
+//Create DOM from virtual nodes
+function renderNode(vNode) {
+    const { nodeName, props, children } = vNode;
+
+    //support class components
+    if (isClass(nodeName)) {
+        const component = new nodeName(props)
+        Object.assign(component, { updater: updateComponent })
+        const element = renderNode(component.render());
+        component.base = element;
+        return element;
+    }
+    //suport functional components 
+    if (isFunction(nodeName)) {
+        return renderNode(nodeName(props))
+    }
+
+    else if (isString(nodeName)) {
+        const element = document.createElement(nodeName)
+
+        handleProps(props, element);
+        handleChildren(children, elements)
+
+        return element;
+    }
+    function handleChildren(children, element) {
+        (children || []).forEach(child => {
+            if (isString(child)) {
+                element.appendChild(document.createTextNode(child))
+
+            } else { element.appendChild(renderNode(child)) }
+        })
+
+
+    }
+    function handleProps(props, element) {
+        for (let propName in props) {
+            // supports events
+            if (isEvent(propName)) {
+                const eventName = propName.substring(2).toLowerCase();
+                element.addEventListener(eventName, props[propName])
+            }
+            // supports DOM props
+            else if (propName in element) {
+                element[propName] = props[propName]
+            }
+            // support custom attributes
+            else {
+                element.setAttribute(propName, props[propsName])
+            }
+        }
+
+    }
+
+    let currentApp;
+
+    function render(element, rootElement) {
+        const app = renderNode(element);
+        currentApp ?
+            rootElement.replaceChild(app, currentApp) :
+            rootElement.appendChild(app);
+        currentApp = app;
+    }
+
+}
+
+export default { render };
